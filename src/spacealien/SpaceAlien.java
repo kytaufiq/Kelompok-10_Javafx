90da425f88fc649d5a0f5313eadab25c08fd125e
zakytaufiq <151290835+kytaufiq@users.noreply.github.com>
2024-12-14 19:56:02 +0700

    Update SpaceAlien.java

---
+++ src/spacealien/SpaceAlien.java
@@ -0,0 +1,30 @@
+package spacealien;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class SpaceAlien extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // Memuat Menu.fxml terlebih dahulu
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Menu.fxml"));
+        Parent root = loader.load();
+
+        // Membuat scene dan menetapkan ukuran
+        Scene scene = new Scene(root, 800, 600);
+
+        // Menetapkan scene ke stage
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Space Alien Game");
+        primaryStage.setResizable(true);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
