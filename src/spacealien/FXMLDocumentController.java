675c3bcca2c0461cd073096d8bab5ddf6708d1f3
Fandrialbara <152765474+Fandrialbara@users.noreply.github.com>
2024-12-14 19:24:41 +0700

    Membuat Abstract class GameEntity

--- src/spacealien/FXMLDocumentController.java
+++ src/spacealien/FXMLDocumentController.java
@@ -19,6 +19,34 @@ import javafx.scene.image.Image;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
+abstract class GameEntity {
+    protected int x;
+    protected int y;
+    protected boolean isActive;
+    
+    public GameEntity(int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.isActive = true;
+    }
+    
+    abstract void update();
+    abstract void render(GraphicsContext gc);
+    
+    public boolean isActive() {
+        return isActive;
+    }
+    
+    public void setActive(boolean active) {
+        this.isActive = active;
+    }
+    
+    public int getX() { return x; }
+    public int getY() { return y; }
+    public void setX(int x) { this.x = x; }
+    public void setY(int y) { this.y = y; }
+}
+
 public class FXMLDocumentController {
 
     private static final Random RAND = new Random();
