b50c961b70d79fbe3ab25c528a6e119b1f8b41c5
zakytaufiq <151290835+kytaufiq@users.noreply.github.com>
2024-12-14 18:10:00 +0700

    membuat shoot,update,draw di kelas rocket

--- src/spacealien/FXMLDocumentController.java
+++ src/spacealien/FXMLDocumentController.java
@@ -44,4 +44,25 @@ public class Rocket {
         img = image;
     }
 
+    public Shot shoot() {
+        return new Shot(posX + size / 2 - Shot.size / 2, posY - Shot.size);
+    }
+
+    public void update() {
+        if (exploding) explosionStep++;
+        destroyed = explosionStep > FXMLDocumentController.EXPLOSION_STEPS;
+    }
+
+    public void draw(GraphicsContext gc) {
+        if (exploding) {
+            gc.drawImage(FXMLDocumentController.EXPLOSION_IMG, 
+                explosionStep % FXMLDocumentController.EXPLOSION_COL * FXMLDocumentController.EXPLOSION_W, 
+                (explosionStep / FXMLDocumentController.EXPLOSION_ROWS) * FXMLDocumentController.EXPLOSION_H + 1,
+                FXMLDocumentController.EXPLOSION_W, FXMLDocumentController.EXPLOSION_H,
+                posX, posY, size, size);
+        } else {
+            gc.drawImage(img, posX, posY, size, size);
+        }
+    }
+
 
