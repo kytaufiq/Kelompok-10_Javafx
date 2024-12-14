8d9cc23e11a655af8a4033608dbb13de04bf7bb4
zakytaufiq <151290835+kytaufiq@users.noreply.github.com>
2024-12-14 18:34:51 +0700

    membuat class shoot

--- src/spacealien/FXMLDocumentController.java
+++ src/spacealien/FXMLDocumentController.java
@@ -81,4 +81,24 @@ public class Rocket {
     }
 }
 
+public class Shot {
+    public boolean toRemove;
+    int posX, posY, speed = 10;
+    static final int size = 6;
+
+    public Shot(int posX, int posY) {
+        this.posX = posX;
+        this.posY = posY;
+    }
+
+    public void update() {
+        posY -= speed;
+    }
+
+    public void draw(GraphicsContext gc) {
+        gc.setFill(Color.RED);
+        gc.fillOval(posX, posY, size, size);
+    }
+}
+
 
